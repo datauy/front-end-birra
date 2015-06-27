bee911cf3b016153d51de1d5287b395b255e85f2
Gustavo Sanchez <antero105@gmail.com>
2015-06-27 19:06:34 -0300

    Map route, navigation directive and nagitavion directive

---
+++ app/scripts/directives/navigation.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name birraApp.directive:navigation
+ * @description
+ * # navigation
+ */
+angular.module('birraApp')
+  .directive('navigation', function () {
+    return {
+      templateUrl: 'views/navigation.html',
+      restrict: 'E',
+      controller: 'controllers/navigation.js'
+    };
+  });
