660a6cab4a5fc32d88c0dc2be0c126cfb0a6a293
Emilio Cristalli <emiliocristalli@gmail.com>
2015-07-12 19:43:27 -0300

    Use active marker when selected

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -39,48 +39,91 @@ angular.module('birraApp')
     });
 
     var icons = {
-      'brewery': 'images/pin-brewery.svg',
-      'Brewpub': 'images/pin-bar.svg',
-      'Tienda': 'images/pin-store.svg',
-      'Bar': 'images/pin-bar.svg',
-      'Restaurant': 'images/pin-bar.svg',
-      'default': 'images/pin-bar.svg',
+      brewery: {
+        default: {
+          normal: 'images/pin-brewery.svg',
+          selected: 'images/pin-selected.svg',
+        }
+      },
+      venue: {
+        Brewpub: {
+          normal: 'images/pin-bar.svg',
+          selected: 'images/pin-selected.svg'
+        },
+        Tienda: {
+          normal: 'images/pin-store.svg',
+          selected: 'images/pin-selected.svg'
+        },
+        Bar: {
+          normal: 'images/pin-bar.svg',
+          selected: 'images/pin-selected.svg'
+        },
+        Restaurant: {
+          normal: 'images/pin-bar.svg',
+          selected: 'images/pin-selected.svg'
+        },
+        default: {
+          normal: 'images/pin-bar.svg',
+          selected: 'images/pin-selected.svg'
+        },
+      },
     };
 
-    function addMarker(model, type, iconUrl) {
+    function addMarker(model, type, subType) {
       if (model.lat !== null && model.lng !== null) {
         $scope.markers[type + '_' + model.id] = {
           lat: parseFloat(model.lat),
           lng: parseFloat(model.lng),
           model: model,
-          icon: {
-            iconUrl: iconUrl,
-            iconSize: [38, 49],
-            iconAnchor:   [19, 42],
-          },
+          icon: iconFor(type, subType, 'normal'),
           type: type,
+          subType: subType,
         };
+      }
+    }
+
+    function iconFor(type, subType, state) {
+      return {
+        iconUrl: (icons[type][subType] || icons[type].default)[state],
+        iconSize: [38, 49],
+        iconAnchor:   [19, 42],
+      };
+    }
+
+    function select(marker) {
+      unselect();
+
+      $scope.selectedMarker = marker;
+      $scope.markers[marker.type + '_' + marker.model.id].icon = iconFor(marker.type, marker.subType, 'selected');
+    }
+
+    function unselect() {
+      if ($scope.selectedMarker) {
+        var marker = $scope.selectedMarker;
+
+        $scope.markers[marker.type + '_' + marker.model.id].icon = iconFor(marker.type, marker.subType, 'normal');
+        $scope.selectedMarker = null;
       }
     }
 
     //Getting breweries
     Brewerie.query(function(breweries) {
       angular.forEach(breweries, function(brewery) {
-        addMarker(brewery, 'brewery', icons.brewery);
+        addMarker(brewery, 'brewery', brewery.brewery_type);
       });
     });
 
     Venue.query(function(venues) {
       angular.forEach(venues, function(venue) {
-        addMarker(venue, 'venue', icons[venue.venue_type] || icons.default);
+        addMarker(venue, 'venue', venue.venue_type);
       });
     });
 
     $scope.$on('leafletDirectiveMarker.click', function(event, args){
-      $scope.selectedMarker = args.model;
+      select(args.model);
     });
 
     $scope.$on('leafletDirectiveMap.click', function(){
-      $scope.selectedMarker = null;
+      unselect();
     });
   });
